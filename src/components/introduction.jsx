9c397f1f25a343f307fd50b9948490fa70c06528
Teo Boufis <theodore_boufis@hotmail.com>
2020-04-16 20:26:26 +0300

    commit 20:26

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -7,7 +7,7 @@ export default class Introduction extends Component {
         <section id="colorlib-hero" className="js-fullheight" data-section="home">
           <div className="flexslider js-fullheight">
             <ul className="slides">
-              <li style={{ backgroundImage: 'url(images/img_bg.jpg)' }}>
+              <li style={{ backgroundImage: 'url(images/img_bg1.jpg)' }}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
@@ -15,43 +15,43 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
                           <h1>Hi! I'm Teo</h1>
-                          <p><a className="btn btn-primary btn-learn" href="">View CV<i className="icon-download4" /></a></p>
+                          <p><a className="btn btn-primary btn-learn" href="/files/TheodorosBoufisCV.pdf" target="_blank" download>Download CV <i className="icon-download4" /></a></p>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </li>
-              <li style={{ backgroundImage: 'url(images/img_bg.jpg)' }}>
+              <li style={{ backgroundImage: 'url(images/img_bg1.jpg)' }}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>I love building things!</h1>
-                          <p><a className="btn btn-primary btn-learn" href="" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
+                          <h4>I love building things!</h4>
+                          <p><a className="btn btn-primary btn-learn" href="#projects">View Projects <i className="icon-briefcase3" /></a></p>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </li>
-              <li style={{ backgroundImage: 'url(images/img_bg.jpg)' }}>
+              {/*<li style={{ backgroundImage: 'url(images/img_bg2.jpg)' }}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h3>I often Write ... </h3>
+                          <h3></h3>
                           <p><a className="btn btn-primary btn-learn" href="" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </li>
+    </li>*/}
             </ul>
           </div>
         </section>
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -28,7 +28,7 @@ export default class Sidebar extends Component {
               <ul>
                 <li><a href="https://www.linkedin.com/in/theodorosboufis/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
                 <li><a href="https://twitter.com/theodorosboufis" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
-                <li><a href="https://www.facebook.com/dhruv.barochia" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
+                <li><a href="https://www.facebook.com/tboufis" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
                 <li><a href="https://www.instagram.com/teoboufis/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
                 {/*<li><a href="https://github.com/Dhruv34788" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
                 <li><a href="https://medium.com/@dhruv.barochia34788" target="_blank" rel="noopener noreferrer"><i className="icon-blogger2"></i></a></li>*/}
